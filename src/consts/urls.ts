2d677523fce66ede69a1edc94d3cc97b60c7cf89
hazardvictor <victor@tokamak.network>
2025-06-12 11:54:58 +0200

    chore: update external urls with public ones

--- src/consts/urls.ts
+++ src/consts/urls.ts
@@ -3,12 +3,12 @@ export const GET_HELP_URL =
   "https://docs.google.com/forms/d/e/1FAIpQLSc6pHHABsnIZ2MvcEa57VCtSNTRXHAaFjBLQYf1LbiMB_81OA/viewform";
 export const TRH_V1_URL = "https://tokamak-rollup-hub.vercel.app";
 export const USER_GUIDE_URL =
-  "https://docs.tokamak.network/home/~/changes/140/service-guide/rollup-hub";
+  "https://docs.tokamak.network/home/service-guide/rollup-hub";
 export const THANOS_STACK_DEPLOYMENT_GUIDE_URL =
-  "https://docs.tokamak.network/home/~/changes/140/service-guide/rollup-hub/rollup-hub-sdkv1/introduction";
+  "https://docs.tokamak.network/home/service-guide/rollup-hub/rollup-hub-sdkv1/introduction";
 export const THANOS_BRIDGE_USER_GUIDE_URL =
-  "https://docs.tokamak.network/home/~/changes/140/service-guide/rollup-hub/rollup-hub-sdkv1/rollup-stack/thanos-stack/integrations/bridge/how-to-use-the-bridge";
+  "https://docs.tokamak.network/home/service-guide/rollup-hub/rollup-hub-sdkv1/rollup-stack/thanos-stack/integrations/bridge/how-to-use-the-bridge";
 export const THANOS_BRIDGE_DEPLOYMENT_GUIDE_URL =
-  "https://docs.tokamak.network/home/~/changes/140/service-guide/rollup-hub/rollup-hub-sdkv1/rollup-stack/thanos-stack/integrations/bridge";
+  "https://docs.tokamak.network/home/service-guide/rollup-hub/rollup-hub-sdkv1/rollup-stack/thanos-stack/integrations/bridge";
 export const THANOS_EXPLORER_DEPLOYMENT_GUIDE_URL =
-  "https://docs.tokamak.network/home/~/changes/140/service-guide/rollup-hub/rollup-hub-sdkv1/rollup-stack/thanos-stack/integrations/block-explorer";
+  "https://docs.tokamak.network/home/service-guide/rollup-hub/rollup-hub-sdkv1/rollup-stack/thanos-stack/integrations/block-explorer";
